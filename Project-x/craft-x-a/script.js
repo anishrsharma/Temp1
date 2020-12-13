7b0831ba16a898845574ee1f6f8d618f2c5873d5
Anish <anishrajsharma06@gmail.com>
2020-12-13 18:12:33 +0530

    initial commit

---
+++ Project-x/craft-x-a/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $('.xyz').click(function() {
+        $('.ul-2').toggleClass('xx');
+    });
+});
+
+
+let clicked = true;
+
+function myFunction() {
+
+    if (clicked) {
+        document.getElementById('a').style.display = 'none';
+        document.getElementById('b').style.display = 'block';
+
+        // document.getElementById('ul2').style.backgroundColor = 'blue';
+
+
+
+    } else {
+        document.getElementById('a').style.display = 'block';
+        document.getElementById('b').style.display = 'none';
+    }
+    clicked = !clicked;
+}
